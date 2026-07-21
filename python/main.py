0c36bbef5272777a7c3f9fe30a6567ce3834a63e
jonathanrobin13 <jonathan.robin13@gmail.com>
2026-07-21 11:45:47 -0400

    simple camera working

--- python/main.py
+++ python/main.py
@@ -1,13 +1,34 @@
 import cv2
-from pathlib import Path
+# from pathlib import Path
 
-project_folder = Path(__file__).parent.parent
+# project_folder = Path(__file__).parent.parent
 
-img_path = project_folder / "assets" / "basketball.jpg"
+# img_path = project_folder / "assets" / "basketball.jpg"
 
-img = cv2.imread(img_path)
+# img = cv2.imread(img_path)
 
-small_img = cv2.resize(img, (800, 600))
-cv2.imshow('Basketball', small_img)
+# small_img = cv2.resize(img, (800, 600))
+# cv2.imshow('Basketball', small_img)
 
-cv2.waitKey(0)
+# cv2.waitKey(0)
+
+cap = cv2.VideoCapture(1)
+
+if not cap.isOpened():
+    print("Could not open camera.")
+    exit()
+
+while True:
+    ret, frame = cap.read()
+
+    if not ret:
+        print("Failed to grab frame.")
+        break
+
+    cv2.imshow("Camera", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
