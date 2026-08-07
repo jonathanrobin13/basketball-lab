ec7896db8b15fdb7975234b43dfe6ee064eee7e4
jonathanrobin13 <jonathan.robin13@gmail.com>
2026-08-07 17:30:16 -0400

    organized tracking systems

--- python/yolo.py
+++ python/yolo.py
@@ -1,25 +1,9 @@
 from ultralytics import YOLO
-import cv2
 from pathlib import Path
-import time
 import shutil
 
+
 project_folder = Path(__file__).parent.parent
-
-# try:
-#     model_path = project_folder / "models" / "yolov8n.pt"
-#     model = YOLO(model_path)
-# except FileNotFoundError:
-
-#     print("This can take a few moments...")
-
-#     model = YOLO("yolov8.pt")
-
-#     new_model_path = project_folder / "models" / "yolov8n.pt"
-
-#     shutil.move(model_path, new_model_path)
-
-#     model_path = new_model_path
 
 model_path = project_folder / "models" / "yolov8n.pt"
 
@@ -37,43 +21,5 @@ else:
 
     model_path = new_model_path
 
-
-video_path = project_folder / "assets" / "real_throw.mp4"
-
-cap = cv2.VideoCapture(1)
-
-fps = 0
-skip_frame = False
-
-while cap.isOpened():
-
-    success, frame = cap.read()
-
-    if success:
-
-        start = time.time()
-        if not skip_frame:
-            results = model.track(source=frame, tracker='bytetrack.yaml', persist=True, classes=[
-                32], verbose=False, conf=0.1)
-
-            annotated_frame = results[0].plot()
-
-            skip_frame = True
-        else:
-            skip_frame = False
-
-        end = time.time()
-        fps = 1 / (end - start)
-
-        cv2.putText(annotated_frame, f"FPS {fps:.1f}", (20, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1
-                    )
-        cv2.imshow("Basketball identification", annotated_frame)
-
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
-
-    else:
-        break
-
-cap.release()
-cv2.destroyAllWindows()
+results = model.track(source=0, conf=0.1, verbose=False,
+                      classes=[32], tracker="bytetrack.yaml")
